aaa339bfed8c1b47b995c8391743cf8ab44e0f40
Leandro Lemes <leandroolemes22@gmail.com>
2024-11-27 21:19:30 -0300

    first commit

---
+++ src/database/ConexaoMySql.js
@@ -0,0 +1,20 @@
+import mysql from "mysql2/promise";
+
+const dbConfig = {
+  host: process.env.MYSQL_HOST || "localhost",
+  port: process.env.MYSQL_PORT || "3306",
+  user: process.env.MYSQL_USER || "root",
+  password: process.env.MYSQL_PWD || "*Ols2024",
+  database: process.env.MYSQL_DB || "api-easy-check",
+ };
+ 
+
+class ConexaoMySql {
+  async getConexao() {
+    if (!ConexaoMySql.conexao) {
+      ConexaoMySql.conexao = await mysql.createConnection(dbConfig);
+    }
+    return ConexaoMySql.conexao;
+  }
+}
+export default ConexaoMySql;
