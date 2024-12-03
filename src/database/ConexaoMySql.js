aeab6b6f662a9787fab3075ee0a276d5b4e6fc3b
Leandro Lemes <leandroolemes22@gmail.com>
2024-12-03 20:30:31 -0300

    atualizado

--- src/database/ConexaoMySql.js
+++ src/database/ConexaoMySql.js
@@ -1,11 +1,11 @@
 import mysql from "mysql2/promise";
 
 const dbConfig = {
-  host: process.env.MYSQL_HOST || "localhost",
-  port: process.env.MYSQL_PORT || "3306",
+  host: process.env.MYSQL_HOST || "autorack.proxy.rlwy.net",
+  port: process.env.MYSQL_PORT || "14036",
   user: process.env.MYSQL_USER || "root",
-  password: process.env.MYSQL_PWD || "*Ols2024",
-  database: process.env.MYSQL_DB || "api-easy-check",
+  password: process.env.MYSQL_PWD || "nDTfJUTEBRtGuwHlLhuEILLQUtTqXLNI",
+  database: process.env.MYSQL_DB || "railway",
  };
  
 
