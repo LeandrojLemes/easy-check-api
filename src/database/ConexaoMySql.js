941e37a8fccc8d4ab500994db2a9c783f7b68964
Leandro Lemes <leandroolemes22@gmail.com>
2025-06-04 20:38:17 -0300

    teste

--- src/database/ConexaoMySql.js
+++ src/database/ConexaoMySql.js
@@ -8,21 +8,21 @@ import mysql from "mysql2/promise";
 //   database: process.env.MYSQL_DB || "railway",
 //  };
 
- const dbConfig = {
-  host: process.env.MYSQL_HOST || "localhost",
-  port: process.env.MYSQL_PORT || "3306",
-  user: process.env.MYSQL_USER || "root",
-  password: process.env.MYSQL_PWD || "PWDwtOPpMdMlEBrerwRPELBtWKniSEPy",
-  database: process.env.MYSQL_DB || "eMySQL",
-};
-
-// const dbConfig = {
-//   host:     process.env.MYSQL_HOST     || "hopper.proxy.rlwy.net",
-//   port:     process.env.MYSQL_PORT     || "49202",
-//   user:     process.env.MYSQL_USER     || "root",
-//   password: process.env.MYSQL_PWD      || "PWDwtOPpMdMlEBrerwRPELBtWKniSEPy",
-//   database: process.env.MYSQL_DB       || "MySQL",
+//  const dbConfig = {
+//   host: process.env.MYSQL_HOST || "localhost",
+//   port: process.env.MYSQL_PORT || "3306",
+//   user: process.env.MYSQL_USER || "root",
+//   password: process.env.MYSQL_PWD || "PWDwtOPpMdMlEBrerwRPELBtWKniSEPy",
+//   database: process.env.MYSQL_DB || "eMySQL",
 // };
+
+const dbConfig = {
+  host:     process.env.MYSQL_HOST     || "hopper.proxy.rlwy.net",
+  port:     process.env.MYSQL_PORT     || "49202",
+  user:     process.env.MYSQL_USER     || "root",
+  password: process.env.MYSQL_PWD      || "PWDwtOPpMdMlEBrerwRPELBtWKniSEPy",
+  database: process.env.MYSQL_DB       || "MySQL",
+};
 
  
 
