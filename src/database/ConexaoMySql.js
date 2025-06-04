791670ef01ce8a73033affcd1d9a4a89cd523b25
LeandrojLemes <144750251+LeandrojLemes@users.noreply.github.com>
2025-06-03 21:28:49 -0300

    Update ConexaoMySql.js

--- src/database/ConexaoMySql.js
+++ src/database/ConexaoMySql.js
@@ -1,12 +1,12 @@
 import mysql from "mysql2/promise";
 
-const dbConfig = {
-  host: process.env.MYSQL_HOST || "autorack.proxy.rlwy.net",
-  port: process.env.MYSQL_PORT || "14036",
-  user: process.env.MYSQL_USER || "root",
-  password: process.env.MYSQL_PWD || "nDTfJUTEBRtGuwHlLhuEILLQUtTqXLNI",
-  database: process.env.MYSQL_DB || "railway",
- };
+// const dbConfig = {
+//   host: process.env.MYSQL_HOST || "autorack.proxy.rlwy.net",
+//   port: process.env.MYSQL_PORT || "14036",
+//   user: process.env.MYSQL_USER || "root",
+//   password: process.env.MYSQL_PWD || "nDTfJUTEBRtGuwHlLhuEILLQUtTqXLNI",
+//   database: process.env.MYSQL_DB || "railway",
+//  };
 
 //  const dbConfig = {
 //   host: process.env.MYSQL_HOST || "localhost",
@@ -15,6 +15,15 @@ const dbConfig = {
 //   password: process.env.MYSQL_PWD || "nDTfJUTEBRtGuwHlLhuEILLQUtTqXLNI",
 //   database: process.env.MYSQL_DB || "easy-check",
 // };
+
+const dbConfig = {
+  host:     process.env.MYSQL_HOST     || "hopper.proxy.rlwy.net",
+  port:     process.env.MYSQL_PORT     || "49202",
+  user:     process.env.MYSQL_USER     || "root",
+  password: process.env.MYSQL_PWD      || "PWDwtOPpMdMlEBrerwRPELBtWKniSEPy",
+  database: process.env.MYSQL_DB       || "railway",
+};
+
  
 
 class ConexaoMySql {
