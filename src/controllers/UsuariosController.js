6036ffd6ed464cd0a5e1ac51c88846aa86a43754
Leandro Lemes <leandroolemes22@gmail.com>
2025-06-23 19:39:41 -0300

    Alteracoes Andre

--- src/controllers/UsuariosController.js
+++ src/controllers/UsuariosController.js
@@ -1,19 +1,103 @@
+// import ConexaoMySql from "../database/ConexaoMySql.js";
+
+// class UsuariosController {
+//   async adicionar(req, res) {
+//     try {
+//       const { nome, email, senha } = req.body;
+//       console.log("nome: ", nome)
+
+//       if (!nome || !email || !senha) {
+//         res.status(400).send("Os campos nome, email e senha são obrigatórios.");
+//         return;
+//       }
+
+//       const conexao = await new ConexaoMySql().getConexao();
+//       const comandoSql = "INSERT INTO usuarios (nome, email, senha) VALUES (?, ?, MD5(?))";
+//       await conexao.execute(comandoSql, [nome, email, senha]);
+
+//       res.status(201).send("Usuário cadastrado com sucesso.");
+//     } catch (error) {
+//       if (error.code === "ER_DUP_ENTRY") {
+//         res.status(400).send("Email já cadastrado.");
+//       } else {
+//         res.status(500).send("Erro ao cadastrar usuário.");
+//       }
+//     }
+//   }
+
+//   async listar(req, res) {
+//     try {
+//       const conexao = await new ConexaoMySql().getConexao();
+//       const [resultado] = await conexao.execute("SELECT id, nome, email FROM usuarios");
+//       res.send(resultado);
+//     } catch (error) {
+//       res.status(500).send("Erro ao listar usuários.");
+//     }
+//   }
+
+//   async atualizar(req, res) {
+//     try {
+//       const { id, nome, email } = req.body;
+
+//       if (!id || !nome || !email) {
+//         res.status(400).send("Os campos id, nome e email são obrigatórios.");
+//         return;
+//       }
+
+//       const conexao = await new ConexaoMySql().getConexao();
+//       const comandoSql = "UPDATE usuarios SET nome = ?, email = ? WHERE id = ?";
+//       await conexao.execute(comandoSql, [nome, email, id]);
+
+//       res.send("Usuário atualizado com sucesso.");
+//     } catch (error) {
+//       res.status(500).send("Erro ao atualizar usuário.");
+//     }
+//   }
+
+//   async excluir(req, res) {
+//     try {
+//       const { id } = req.params;
+//       const conexao = await new ConexaoMySql().getConexao();
+//       const comandoSql = "DELETE FROM usuarios WHERE id = ?";
+//       await conexao.execute(comandoSql, [id]);
+
+//       res.send("Usuário excluído com sucesso.");
+//     } catch (error) {
+//       res.status(500).send("Erro ao excluir usuário.");
+//     }
+//   }
+// }
+
+// export default UsuariosController;
+
+
 import ConexaoMySql from "../database/ConexaoMySql.js";
 
 class UsuariosController {
   async adicionar(req, res) {
     try {
-      const { nome, email, senha } = req.body;
+      const { cnpj, nome_empresa, nome, email, senha } = req.body;
       console.log("nome: ", nome)
 
-      if (!nome || !email || !senha) {
-        res.status(400).send("Os campos nome, email e senha são obrigatórios.");
+      if (!cnpj || !nome_empresa || !nome || !email || !senha) {
+        res.status(400).send("Os campos Nome da Empresa, CNPJ, nome, email e senha são obrigatórios.");
+        return;
+      }
+
+      const cnpjLimpo = cnpj.replace(/\D/g, '');
+      if (cnpjLimpo.length !== 14) {
+        res.status(400).send('CNPJ inválido. Deve conter 14 dígitos.');
+        return;
+      }
+
+      if (!email.includes('@') || !email.includes('.com')) {
+        res.status(400).send("Formato de Email inválido!");
         return;
       }
 
       const conexao = await new ConexaoMySql().getConexao();
-      const comandoSql = "INSERT INTO usuarios (nome, email, senha) VALUES (?, ?, MD5(?))";
-      await conexao.execute(comandoSql, [nome, email, senha]);
+      const comandoSql = "INSERT INTO usuarios (cnpj, nome_empresa, nome, email, senha) VALUES (?, ?, ?, ?, MD5(?))";
+      await conexao.execute(comandoSql, [cnpj, nome_empresa, nome, email, senha]);
 
       res.status(201).send("Usuário cadastrado com sucesso.");
     } catch (error) {
@@ -24,6 +108,7 @@ class UsuariosController {
       }
     }
   }
+
 
   async listar(req, res) {
     try {
