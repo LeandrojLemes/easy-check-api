de7eb4a2a05453ec4432c32cb7acc2bd920146d4
Leandro Lemes <leandroolemes22@gmail.com>
2025-06-24 21:11:56 -0300

    Limpeza de código

--- src/database/ConexaoMySql.js
+++ src/database/ConexaoMySql.js
@@ -1,21 +1,5 @@
 import mysql from "mysql2/promise";
 
-// const dbConfig = {
-//   host: process.env.MYSQL_HOST || "autorack.proxy.rlwy.net",
-//   port: process.env.MYSQL_PORT || "14036",
-//   user: process.env.MYSQL_USER || "root",
-//   password: process.env.MYSQL_PWD || "nDTfJUTEBRtGuwHlLhuEILLQUtTqXLNI",
-//   database: process.env.MYSQL_DB || "railway",
-//  };
-
-//  const dbConfig = {
-//   host: process.env.MYSQL_HOST || "localhost",
-//   port: process.env.MYSQL_PORT || "3306",
-//   user: process.env.MYSQL_USER || "root",
-//   password: process.env.MYSQL_PWD || "PWDwtOPpMdMlEBrerwRPELBtWKniSEPy",
-//   database: process.env.MYSQL_DB || "eMySQL",
-// };
-
 const dbConfig = {
   host:     process.env.MYSQL_HOST     || "hopper.proxy.rlwy.net",
   port:     process.env.MYSQL_PORT     || "49202",
